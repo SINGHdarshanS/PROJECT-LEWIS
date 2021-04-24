a2075ce8f79b03923ed4db22081672796ee92612
Darshan S <dssinghm699@gmail.com>
2021-04-24 01:27:35 -0400

    server master made callable and unnecessary files removed

--- base_station/code/server_master.py
+++ base_station/code/server_master.py
@@ -3,31 +3,35 @@ from tqdm import tqdm
 import os
 import signal
 import sys
+import subprocess
+from time import sleep
 
 def signal_handler(sig, frame):
     print('Connection Aborted. Socket being terminated remotely')
     s.shutdown(2)
     s.close()
     sys.exit('Socket connection terminated')
+def receive_video():
+    PATH = "/home/darshy/PROJECT-LEWIS/base_station/footage/"
 
-PATH = "/home/darshy/Capstone/footage/"
+    signal.signal(signal.SIGINT, signal_handler)
+    SERVER_HOST = "192.168.8.240"
+    SERVER_PORT = 8000
+    BUFFER_SIZE = 4096
+    SEPARATOR = "<SEPARATOR>"
 
-signal.signal(signal.SIGINT, signal_handler)
-SERVER_HOST = "192.168.8.233"
-SERVER_PORT = 8000
-BUFFER_SIZE = 4096
-SEPARATOR = "<SEPARATOR>"
+    s = socket()
+    s.bind((SERVER_HOST, SERVER_PORT))
 
-s = socket()
-s.bind((SERVER_HOST, SERVER_PORT))
-s.listen(5)
-print('listening')
+    s.listen(5)
+    print('listening')
 
-client_socket, address = s.accept()
-print('{} is connected'.format(address))
+    client_socket, address = s.accept()
+    print('{} is connected'.format(address))
 
-while True:
+# while True:
     received = client_socket.recv(BUFFER_SIZE).decode()
+    print(received.split(SEPARATOR))
     filename, filesize = received.split(SEPARATOR)
     filename = os.path.basename(filename) # look up what basename() does. I believe it decodes something but I'm not sure
 
@@ -36,12 +40,18 @@ while True:
     filesize = int(filesize)
 
 #    progress = tqdm(range(filesize), "receiving {}".format(filename), unit="B", unit_scale=True, unit_divisor=1024)
-    with open(PATH + filename, "wb") as f:
-        progress = tqdm(range(filesize), "receiving {}".format(filename), unit="B", unit_scale=True, unit_divisor=1024)
+    if os.path.exists(PATH+'1sec.h264'):
+        os.remove(PATH+'1sec.h264')
+        if os.path.exists(PATH+'process.mp4'):
+            os.remove(PATH+'process.mp4')
+    with open(PATH + '1sec.h264', "wb") as f:
         while True:
             bytes_read = client_socket.recv(BUFFER_SIZE)
             if not bytes_read:
 #                print('got here')
                 break
             f.write(bytes_read)
-  progress.update(len(bytes_read))
+        print('transfer complete')
+    subprocess.check_output("MP4Box -add {} {}".format(PATH+"1sec.h264", PATH+"process.mp4"), stderr=subprocess.STDOUT, shell=True)
+    print("processing complete")
+    return PATH+'process.mp4'
